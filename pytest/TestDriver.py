04e14672369434b38d4c8257b8d06862495899ff
Hans Kammler <coolmacmaniac@gmail.com>
2018-04-18 02:38:40 +0530

    Added GET request test case
    Added test case to make a GET request and fetch its response as text.

---
+++ pytest/TestDriver.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Created on  : Tue Apr 17 23:39:14 2018
+@author     : Sourabh
+"""
+
+# %%
+
+import sys
+
+# add modules path before importing them
+sys.path.insert(0, '../../')
+    
+from pyutils.web.HttpRequest import HttpRequest
+from pyutils.web.HttpMethod import HttpMethod
+from pyutils.web.HttpRequestManager import HttpRequestManager
+
+class TestDriver:
+    
+    def __init__(self):
+        pass
+    
+    def testWebRequest(self):
+        req = HttpRequest()
+        req.uri = 'http://date.jsontest.com/'
+        
+        reqMgr = HttpRequestManager()
+        rsp = reqMgr.invoke(req, HttpMethod.GET)
+        
+        print(rsp.payload)
+        pass
+    
+    pass
+
+if __name__ == '__main__':
+    td = TestDriver()
+    td.testWebRequest()
+    pass
